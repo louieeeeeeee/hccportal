c8ca2e4bfbf8c19bcb077378580e45f9a77f5896
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-05 05:35:27 +0800

    Update report_body.php

--- admin/schedule/dist/includes/report_body.php
+++ admin/schedule/dist/includes/report_body.php
@@ -1,9 +1,4 @@
 
-
-
-<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
 <table  style="width:100%;float:left;font-size:15px;" class="table table-hover">
 							<thead>
 							  <tr>
@@ -198,8 +193,7 @@
 							
 		<?php }?>					  
 		</table>    
-		</div>
-    <div class="carousel-item">
+
 			<table  style="width:100%;float:left;font-size:15px;" class="table table-hover">
 								<thead>
 								   <br> <br> <br> <br> <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
@@ -393,16 +387,5 @@
 			
 			<?php }?>					  
 			</table>
-			</div>
-			</div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
+			
 			
