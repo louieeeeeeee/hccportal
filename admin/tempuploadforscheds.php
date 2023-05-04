ce681c34e9bcad7e0d50184ffe1a7dffb1a74d91
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-05 03:14:00 +0800

    Update tempuploadforscheds.php

--- admin/tempuploadforscheds.php
+++ admin/tempuploadforscheds.php
@@ -1,29 +1,12 @@
 <?php
 if(isset($_POST['submit'])){
   $file = $_FILES['file']['tmp_name'];
-  include '../../vendor/autoload.php';
-  
+  include '../vendor/autoload.php';
   $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
   $worksheet = $spreadsheet->getActiveSheet();
+  $rows = $worksheet->toArray();
 
-  // Get starting row and column from textboxes
-  $startRow = isset($_POST['startRow']) ? $_POST['startRow'] : 1;
-  $startCol = isset($_POST['startCol']) ? $_POST['startCol'] : 'A';
-
-  // Convert column letter to index (e.g. A -> 1, B -> 2, etc.)
-  $startColIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($startCol);
-
-  $rows = [];
-
-  echo \PhpOffice\PhpSpreadsheet\Spreadsheet::VERSION;
-  foreach($worksheet->getRowIterator($startRow) as $row){
-    $rowData = $row->toArray(null, false, false);
-    // Skip columns before the starting column
-    $rowData = array_slice($rowData, $startColIndex - 1);
-    $rows[] = $rowData;
-  }
-
-  include '../../config.php';
+  include '../config.php';
 
   foreach($rows as $row){
     $studentid = $row[0];
@@ -40,8 +23,6 @@ if(isset($_POST['submit'])){
 }
 ?>
 <form method="post" enctype="multipart/form-data">
-  <input type="file" name="file" id="file"><br><br>
-  Starting row: <input type="text" name="startRow" value="1"><br>
-  Starting column: <input type="text" name="startCol" value="A"><br><br>
+  <input type="file" name="file" id="file">
   <input type="submit" name="submit" value="Upload">
 </form>
