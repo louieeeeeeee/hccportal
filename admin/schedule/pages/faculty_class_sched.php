f3fbda05eddc23ac84db0e6431c51a7a4fcd0100
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-05 05:44:40 +0800

    Update faculty_class_sched.php

--- admin/schedule/pages/faculty_class_sched.php
+++ admin/schedule/pages/faculty_class_sched.php
@@ -7,7 +7,9 @@ endif;?>
 <html>
 <head>
 <link rel="stylesheet" href="../dist/css/print.css" media="print">
-<script src="../dist/js/jquery.min.js"></script>
+<!-- jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
 <!-- Bootstrap 3 CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
