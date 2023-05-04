3beaa2fbe1d942b05cc971cc1607777481bbcff0
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-05 06:53:01 +0800

    Update report_body.php

--- admin/schedule/dist/includes/report_body.php
+++ admin/schedule/dist/includes/report_body.php
@@ -1,4 +1,28 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<style>
+.carousel-control-prev-icon,
+.carousel-control-next-icon {
+  background-color: #000;
+}
 
+</style>
+
+<div class="container">
+	<h2>Bootstrap Carousel Example</h2>
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ul class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ul>
+
+		<!-- The slideshow -->
+		<div class="carousel-inner">
+			<div class="carousel-item active">
 <table  style="width:100%;float:left;font-size:15px;" class="table table-hover">
 							<thead>
 							  <tr>
@@ -192,7 +216,9 @@
 							  </tr>
 							
 		<?php }?>					  
-		</table>    
+		</table>
+			</div>
+			<div class="carousel-item">   
 
 			<table  style="width:100%;float:left;font-size:15px;" class="table table-hover">
 								<thead>
@@ -387,5 +413,19 @@
 			
 			<?php }?>					  
 			</table>
+			</div>
+		</div>
+
+		<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	<span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+	<span class="carousel-control-next-icon" aria-hidden="true"></span>
+	<span class="sr-only">Next</span>
+</a>
+
+</div>
+</div>
 			
 			
