7db23a56ca56a7c9d0764a7fc52348834d185ff4
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-06 19:49:44 +0800

    Update mainte_sy.php

--- admin/schedule/pages/mainte_sy.php
+++ admin/schedule/pages/mainte_sy.php
@@ -134,5 +134,38 @@ error_reporting(0);
 		//-------------------------------------------------------------
 		
 </script>
+
+	<script type="text/javascript" src="autosum.js"></script>
+    <!-- jQuery 2.1.4 -->
+    <script src="../plugins/jQuery/jQuery-2.1.4.min.js"></script>
+	<script src="../dist/js/jquery.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../plugins/select2/select2.full.min.js"></script>
+    <!-- SlimScroll -->
+    <script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+    <!-- FastClick -->
+    <script src="../plugins/fastclick/fastclick.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../dist/js/app.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../dist/js/demo.js"></script>
+    <script src="../plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="../plugins/datatables/dataTables.bootstrap.min.js"></script>
+    
+    <script>
+      $(function () {
+        $("#example1").DataTable();
+        $('#example2').DataTable({
+          "paging": true,
+          "lengthChange": false,
+          "searching": false,
+          "ordering": true,
+          "info": true,
+          "autoWidth": false
+        });
+      });
+    </script>
+   
   </body>
 </html>
