fe4f36887a7649eb3135c9932912dd3fe1bad18b
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-06 18:18:49 +0800

    Update mainte_home.php

--- admin/schedule/pages/mainte_home.php
+++ admin/schedule/pages/mainte_home.php
@@ -149,7 +149,7 @@ include '../../../config.php';
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	<div class="back-button">
-  <a href="../dashboard/dashboard.php">
+  <a href="../index.php">
     <button class="btn btn-primary btn-lg">Back</button>
   </a>
 </div>
