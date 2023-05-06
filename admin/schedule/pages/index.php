682cf6d524e8dcdac4f2432f8375ec5f2b98803b
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-06 10:16:27 +0800

    a

---
+++ admin/schedule/pages/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Centered Big Square Buttons</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	<style>
+		.btn-sq-lg {
+			width: 220px;
+			height: 200px;
+			border-radius: 25px;
+			position: relative;
+			overflow: hidden;
+		}
+		#btn-schedule{
+			border-radius: 25px;
+			color: #2596be;
+		}
+		.fa-6x {
+			padding-top: 45px;
+			position: absolute;
+			top: 0;
+			left: 50%;
+			transform: translateX(-50%);
+			color: #2596be;
+			opacity: 0.8;
+			transition: all 0.2s ease-in-out;
+		}
+		.btn-sq-lg:hover .fa-6x {
+			transform: translate(-50%, -10%) scale(1.2);
+			opacity: 1;
+		}
+		.btn-label {
+			position: absolute;
+			bottom: 0;
+			left: 50%;
+			transform: translateX(-50%);
+			font-size: 20px;
+			font-weight: bold;
+			color: #2596be;
+			opacity: 0.8;
+			transition: all 0.2s ease-in-out;
+		}
+		.btn-sq-lg:hover .btn-label {
+			transform: translate(-50%, 10%);
+			opacity: 1;
+		}
+		.btn-sq-lg:focus {
+			box-shadow: none;
+			outline: none;
+			border-color: #2596be;
+		}
+		.btn-sq-lg:focus .fa-6x, .btn-sq-lg:focus .btn-label {
+			color: #2596be;
+		}
+		body, html {
+			height: 100%;
+			margin: 0;
+			padding: 0;
+		}
+		.container {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			height: 100%;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-4">
+				<button id="btn-schedule" class="btn btn-sq-lg mx-auto">
+					<i class="fas fa-calendar-alt fa-6x"></i>
+					<div class="btn-label">Schedule</div>
+				</button>
+			</div>
+			<div class="col-xs-4">
+				<button id="btn-maintenance" class="btn btn-sq-lg mx-auto">
+					<i class="fas fa-tools fa-6x"></i>
+					<div class="btn-label">Maintenance</div>
+				</button>
+			</div>
+			<div class="col-xs-4">
+				<button id="btn-schedule2" class="btn btn-sq-lg mx-auto">
+					<i class="fas fa-clock fa-6x"></i>
+					<div class="btn-label">Settings</div>
+				</button>
+			</div>
+		</div>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
