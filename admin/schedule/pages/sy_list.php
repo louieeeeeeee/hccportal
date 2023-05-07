57bc0b5d6100236ab6c79a0550858d3fb712f4bc
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-07 09:29:35 +0800

    Update sy_list.php

--- admin/schedule/pages/sy_list.php
+++ admin/schedule/pages/sy_list.php
@@ -1,5 +1,5 @@
 
-<table class="table table-bordered table-striped" style="margin-right:-10px">
+<table id="example1" class="table table-bordered table-striped" style="margin-right:-10px">
 							<thead>
 							  <tr>
 								<th>School Year</th>
