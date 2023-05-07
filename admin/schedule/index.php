301fcc948794327d075239c1f7b20c2eebc06487
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-08 01:28:11 +0800

    Update index.php

--- admin/schedule/index.php
+++ admin/schedule/index.php
@@ -11,7 +11,6 @@ include '../../../config.php';
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Centered Big Square Buttons</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 	<style>
