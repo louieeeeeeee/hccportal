0b4f2683bf18d444603e2986283640d63dece9ec
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-07 10:02:51 +0800

    Update sy_list.php

--- admin/schedule/pages/sy_list.php
+++ admin/schedule/pages/sy_list.php
@@ -20,7 +20,7 @@
 		<td><?php echo $sy;?></td>
 			
 		<td>
-		<a id="click" href="sy.php?id=<?php echo $id;?>&sy=<?php echo $sy;?>">
+		<a id="click" href="mainte_sy.php?id=<?php echo $id;?>&sy=<?php echo $sy;?>">
 		<i class="glyphicon glyphicon-edit text-blue"></i></a>
 		<a id="removeme" href="sy_del.php?id=<?php echo $id;?>">
 		<i class="glyphicon glyphicon-remove text-red"></i></a>
