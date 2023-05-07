13234539e48c16821c468623ba6b8162fc122363
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-07 10:06:39 +0800

    a

--- admin/schedule/pages/mainte_sy.php
+++ admin/schedule/pages/mainte_sy.php
@@ -102,36 +102,32 @@ error_reporting(0);
                       <div class="row">
                         <div class="col-md-12">
                           <div class="result" id="form">
-                          <table id="example1" class="table table-bordered table-striped" style="margin-right:-10px">
-	<thead>
-		<tr>
-		<th>School Year</th>
-		<th>Action</th>
-		
-		
-		</tr>
-	</thead>						
-	<?php
-		include('../dist/includes/dbcon.php');
-		$query=mysqli_query($con,"select * from sy order by sy desc")or die(mysqli_error());
-				
-		while($row=mysqli_fetch_array($query)){
-			$id=$row['sy_id'];
-			$sy=$row['sy'];
-	?>
-		<tr>
-		<td><?php echo $sy;?></td>
-			
-		<td>
-		<a id="click" href="mainte_sy.php?id=<?php echo $id;?>&sy=<?php echo $sy;?>">
-		<i class="glyphicon glyphicon-edit text-blue"></i></a>
-		<a id="removeme" href="sy_del.php?id=<?php echo $id;?>">
-		<i class="glyphicon glyphicon-remove text-red"></i></a>
-		</td>
-
-		</tr>				
-<?php }?>					  
-</table>  
+                            <table id="example1" class="table table-bordered table-striped" style="margin-right:-10px">
+                                <thead>
+                                    <tr>
+                                    <th>School Year</th>
+                                    <th>Action</th>
+                                    </tr>
+                                </thead>						
+                                    <?php
+                                        include('../dist/includes/dbcon.php');
+                                        $query=mysqli_query($con,"select * from sy order by sy desc")or die(mysqli_error());
+                                                
+                                        while($row=mysqli_fetch_array($query)){
+                                            $id=$row['sy_id'];
+                                            $sy=$row['sy'];
+                                    ?>
+		                            <tr>
+                                    <td><?php echo $sy;?></td>	
+                                    <td>
+                                    <a id="click" href="mainte_sy.php?id=<?php echo $id;?>&sy=<?php echo $sy;?>">
+                                    <i class="glyphicon glyphicon-edit text-blue"></i></a>
+                                    <a id="removeme" href="sy_del.php?id=<?php echo $id;?>">
+                                    <i class="glyphicon glyphicon-remove text-red"></i></a>
+                                    </td>
+                                    </tr>				
+                            <?php }?>					  
+                            </table>  
 
                           </div>
                         </div><!--col end -->
