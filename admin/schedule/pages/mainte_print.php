56d09ac4a57af82013e315afdebaa76fb88c4976
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-10 02:47:01 +0800

    Update mainte_print.php

--- admin/schedule/pages/mainte_print.php
+++ admin/schedule/pages/mainte_print.php
@@ -11,13 +11,20 @@ include('../dist/includes/dbcon.php');
 <!DOCTYPE html>
 <html>
 <head>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
+<!-- Select2 CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css"/>
+
+<!-- jQuery library -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Select2 JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
+
 	<style>
         body {
             background: url(../dist/img/temp.jpg) center/cover no-repeat;
@@ -279,6 +286,6 @@ var clockElement = document.getElementById('clock');
 
     <script>
     $(document).ready(function() {
-        $('#faculty').select2();
+        $('.select2').select2();
     });
 </script>
