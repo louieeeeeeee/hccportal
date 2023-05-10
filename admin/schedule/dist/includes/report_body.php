b0fa8f72ee8287e1c9d95add934b34c415c3e845
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-10 15:34:17 +0800

    a

--- admin/schedule/dist/includes/report_body.php
+++ admin/schedule/dist/includes/report_body.php
@@ -316,7 +316,7 @@
 											<div class="show">
 											<ul>
 												<li class="options" style="display:'.$options.'">
-													<span style="float:left;"><a href="sched_edit.php?id='.$id1.'" class="edit" title="Edit">Edit</a></span>
+													<span style="float:left;"><a href="mainte_sched_edit.php?id='.$id1.'" class="edit" title="Edit">Edit</a></span>
 													<span class="action"><a href="#" id="'.$id1.'" class="delete" title="Delete">Remove</a></span>
 												</li></ul>
 											
