db2850cb5304d9a1253d665945d6bc1e6a47df96
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-17 21:14:55 +0800

    Update submit.php

--- admin/schedule/pages/submit.php
+++ admin/schedule/pages/submit.php
@@ -22,6 +22,7 @@ include('../dist/includes/dbcon.php');
 	$t = $_POST['t'];
 	$th = $_POST['th'];
 	$s = $_POST['s'];
+	$s = $_POST['u'];
 	
 	$set_id=$_SESSION['settings'];
 	$program=$_SESSION['id'];
@@ -595,6 +596,103 @@ include('../dist/includes/dbcon.php');
 			<table width='100%'>
 				<tr>
 					<td>saturday</td>
+					<td>$timet</td> 
+					
+					<td>Success</td>					
+				</tr>
+			</table></span><br>";	
+		}
+					
+		else if ($count_t>0) echo $error_t."<br>";
+		else if ($count_r>0) echo $error_r."<br>";
+		else {echo $error_c."<br>";}	
+		
+		//echo "</table>";
+	}
+
+	//sunday sched
+	foreach ($u as $daym){
+		//check conflict for member
+		$query_member=mysqli_query($con,"select *,COUNT(*) as count from schedule 
+		natural join faculty natural join time where facultyid='$member' and schedule.time_id='$daym' and day='u'")or die(mysqli_error($con));
+			$row=mysqli_fetch_array($query_member);
+			$count_t=$row['count'];
+			$time1=date("h:i a",strtotime($row['time_start']))."-".date("h:i a",strtotime($row['time_end']));
+			$member1=$row['lastname'].", ".$row['firstname'];
+			$subjectCode = $row['subject_code'];
+			
+			$error_t="<span class='text-danger'>
+			<table width='100%'>
+				<tr>	
+					<td>sunday</td>
+					<td>$time1</td> 
+					<td>$member1</td>
+					<td class='text-danger'><b>Faculty Not Available</b></td>	
+					<td>$subjectCode</td>
+				</tr>
+				</span>
+			</table>";
+		
+		//check conflict for room
+		$query_room=mysqli_query($con,"select *,COUNT(*) as count from schedule 
+		natural join faculty natural join time where room='$room' and schedule.time_id='$daym' and day='u' and room not like 'TBA'")or die(mysqli_error($con));
+			$rowr=mysqli_fetch_array($query_room);
+			$count_r=$rowr['count'];
+			$timer=date("h:i a",strtotime($rowr['time_start']))."-".date("h:i a",strtotime($rowr['time_end']));
+			$roomr=$rowr['room'];
+			$subjectCode = $row['subject_code'];
+			
+			$error_r="<span class='text-danger'>
+			<table width='100%'>
+				<tr>
+					<td>sunday</td>
+					<td>$timer</td> 
+					<td>Room $roomr</td>
+					<td class='text-danger'><b>Room Not Available</b></td>	
+					<td>$subjectCode</td>
+				</tr>
+				</span>
+			</table>";
+			//check conflict for class
+		$query_class=mysqli_query($con,"select *,COUNT(*) as count from schedule 
+		natural join faculty natural join time where cys='$cys' and schedule.time_id='$daym' and day='u'")or die(mysqli_error($con));
+			$rowc=mysqli_fetch_array($query_class);
+			$count_c=$rowc['count'];
+			$cysc=$rowc ['cys'] ;
+			$timec=date("h:i a",strtotime($rowc['time_start']))."-".date("h:i a",strtotime($rowc['time_end']));
+			$subjectCode = $row['subject_code'];
+			
+			$error_c="<span class='text-danger'>
+			<table width='100%'>
+				<tr>
+					<td>sunday</td>
+					<td>$timec</td> 
+					<td>$cysc</td>
+					<td class='text-danger'><b>Class Not Available</b>	</td>	
+					<td>$subjectCode</td>
+				</tr>
+			</table></span>";	
+		
+				
+		$queryt=mysqli_query($con,"select * from faculty where facultyid='$member'")or die(mysqli_error($con));
+				$rowt=mysqli_fetch_array($queryt);
+				$membert=$rowt['lastname'].", ".$rowt['firstname'];
+			
+		$querytime=mysqli_query($con,"select * from time where time_id='$daym'")or die(mysqli_error($con));
+				$rowt=mysqli_fetch_array($querytime);
+				$timet=date("h:i a",strtotime($rowt['time_start']))."-".date("h:i a",strtotime($rowt['time_end']));	
+		
+		
+		if (($count_t==0) and ($count_r==0) and ($count_c==0))
+		{
+			mysqli_query($con,"INSERT INTO schedule(time_id,day,facultyid,subject_code,cys,room,remarks,settings_id,encoded_by,classtype) 
+				VALUES('$daym','u','$member','$subject','$cys','$room','$remarks','$set_id','$program','$options')")or die(mysqli_error());
+				
+				
+			echo "<span class='text-success'>
+			<table width='100%'>
+				<tr>
+					<td>sunday</td>
 					<td>$timet</td> 
 					
 					<td>Success</td>					
