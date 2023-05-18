7047126b63db62078e6002da486d3af4fdbcc808
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-18 09:32:07 +0800

    Update index.php

--- admin/schedule/index.php
+++ admin/schedule/index.php
@@ -121,7 +121,7 @@ include '../../../config.php';
 	</nav>
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-6">
+			<div class="col-xs-4">
 				<a href="pages/mainte_sched.php">
 					<button id="btn-schedule" class="btn btn-sq-lg mx-auto">
 						<i class="fas fa-calendar-alt fa-6x"></i>
@@ -129,7 +129,7 @@ include '../../../config.php';
 					</button>
 				</a>
 			</div>
-			<div class="col-xs-6">
+			<div class="col-xs-4">
 				<a href="pages/mainte_home.php">
 					<button id="btn-maintenance" class="btn btn-sq-lg mx-auto">
 						<i class="fas fa-wrench fa-6x"></i>
@@ -137,10 +137,10 @@ include '../../../config.php';
 					</button>
 				</a>
 			</div>
-			<div class="col-xs-6">
+			<div class="col-xs-4">
 				<a href="pages/mainte_print.php">
 					<button id="btn-maintenance" class="btn btn-sq-lg mx-auto">
-						<i class="fas fa-wrench fa-6x"></i>
+						<i class="fas fa-print fa-6x"></i>
 						<div class="btn-label">Print</div>
 					</button>
 				</a>
