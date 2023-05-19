ef47cc9d7e0786c57056a31e121fb457eb8e8524
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-19 10:45:15 +0800

    Update report_body.php

--- admin/schedule/dist/includes/report_body.php
+++ admin/schedule/dist/includes/report_body.php
@@ -111,25 +111,27 @@
 										$query3=mysqli_query($con,$test)or die(mysqli_error($con));
 										$row3=mysqli_fetch_array($query3);
 										//echo $test;
-											echo '
-											<div class="show">
-												<div style="background-color: '.$row3["subjcolor"].';">
-													<ul>
-														<li class="options" style="display:'.$options.'">
-															<span  style="float:left;"><a href="mainte_sched_edit.php?id='.$id1.'" class="edit" title="Edit">Edit</a></span>
-															<span class="action"><a href="#" id="'.$id1.'" class="delete" title="Delete">Remove</a></span>
-														</li>
-													</ul>
-													<ul>
-														<li class="showme">
+										echo '
+										<div class="show">
+											<div style="background-color: '.$row3["subjcolor"].';">
+												<ul>
+													<li class="options" style="display:'.$options.'">
+														<span style="float:left;"><a href="mainte_sched_edit.php?id='.$id1.'" class="edit" title="Edit" onclick="window.print();return false;">Edit</a></span>
+														<span class="action"><a href="#" id="'.$id1.'" class="delete" title="Delete" onclick="window.print();return false;">Remove</a></span>
+													</li>
+												</ul>
+												<ul>
+													<li class="showme">
 														<li>'.$row1["subject_code"].'</li>
 														<li class="'.$displayc.'">'.$row1['cys'].'</li>
-														<li class="'.$displaym.'">'.$row1['lastname'].', '.$row1['firstname'].'</li>										
+														<li class="'.$displaym.'">'.$row1['lastname'].', '.$row1['firstname'].'</li>                                      
 														<li class="'.$displayr.'">Room '.$row1['room'].'</li>
 															'.$displayrm.'
-													</ul>
-												</div>
-											</div>';
+													</li>
+												</ul>
+											</div>
+										</div>';
+									
 										}	
 									?>
 								</td>
