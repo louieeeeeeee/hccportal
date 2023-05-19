136f817c3e7d8b566993176f8d63ddb7f4da67aa
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-19 11:30:30 +0800

    a

--- admin/schedule/pages/faculty_sched.php
+++ admin/schedule/pages/faculty_sched.php
@@ -68,6 +68,7 @@ $settings=mysqli_query($con,"select * from settings where settings_id='$sid'")or
 	$rows=mysqli_fetch_array($settings);
 
 	include('../dist/includes/report_header.php');
+  echo '<br><button onclick="printSchedule()">Print Schedule</button>';
 	include('../dist/includes/report_body.php');
 	include('../dist/includes/report_footer.php');
 ?> 
@@ -76,3 +77,59 @@ $settings=mysqli_query($con,"select * from settings where settings_id='$sid'")or
   </body>
 
   </html>
+
+  <script>
+	function printSchedule() {
+	  var monWedTable = document.getElementById("monWedTable").cloneNode(true);
+	  var thuSatTable = document.getElementById("thuSatTable").cloneNode(true);
+
+	  // Set display none for elements with class "action" and "edit"
+	  var actionElements = monWedTable.querySelectorAll('.action');
+	  var editElements = monWedTable.querySelectorAll('.edit');
+	  for (var i = 0; i < actionElements.length; i++) {
+	    actionElements[i].style.display = "none";
+	  }
+	  for (var i = 0; i < editElements.length; i++) {
+	    editElements[i].style.display = "none";
+	  }
+
+	  actionElements = thuSatTable.querySelectorAll('.action');
+	  editElements = thuSatTable.querySelectorAll('.edit');
+	  for (var i = 0; i < actionElements.length; i++) {
+	    actionElements[i].style.display = "none";
+	  }
+	  for (var i = 0; i < editElements.length; i++) {
+	    editElements[i].style.display = "none";
+	  }
+
+	  var newWin = window.open('', 'Print-Window');
+	  newWin.document.open();
+	  newWin.document.write('<html><head><style>table, td, th {border: 1px solid black; border-collapse: collapse;} td, th {padding: 5px;}</style></head><body>');
+	  newWin.document.write('<h1>Faculty Schedule</h1>');
+
+	  // Add the custom HTML code at the top of the table
+	  newWin.document.write('<div>');
+	  newWin.document.write('<span style="margin-right: 5px"><?php echo $text;?>: </span>');
+	  newWin.document.write('<span style="color: blue; margin-right: 15px"><?php echo $value;?></span>');
+	  newWin.document.write('<span style="margin-right: 5px;">School Year:</span>');
+	  newWin.document.write('<span style="color: blue; margin-right: 15px"><?php echo $rows['sy']; ?></span>');
+	  newWin.document.write('<span style="margin-right: 5px">Semester: </span>');
+	  newWin.document.write('<span style="color: blue; margin-right: 15px"><?php echo $rows['sem']; ?></span>');
+	  newWin.document.write('</div>');
+
+	  newWin.document.write(monWedTable.outerHTML);
+
+	  // Add a page break before the thuSatTable
+	  newWin.document.write('<div style="page-break-before: always;"></div>');
+
+	  newWin.document.write('<h1>Thursday-Sunday Schedule</h1>');
+	  newWin.document.write(thuSatTable.outerHTML);
+	  newWin.document.write('</body></html>');
+	  newWin.document.close();
+
+	  setTimeout(function() {
+	    newWin.print();
+	    newWin.close();
+	  }, 100);
+	}
+</script>
