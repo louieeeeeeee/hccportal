e1d63c4df48c2bde9954d67d6112a4949b0a4447
louieeeeeeee <87273125+louieeeeeeee@users.noreply.github.com>
2023-05-19 11:16:19 +0800

    Update class_sched.php

--- admin/schedule/pages/class_sched.php
+++ admin/schedule/pages/class_sched.php
@@ -10,19 +10,6 @@ endif;?>
 <script src="../dist/js/jquery.min.js"></script>
 
 </head>
-<style>
-
-  table {
-    border-collapse: collapse;
-  }
-  td, th {
-    border: 1px solid black;
-    padding: 5px;
-    background-color: inherit !important;
-  }
-}
-	</style>
-
 <body>
 <?php 
 include('../dist/includes/dbcon.php');
@@ -83,53 +70,3 @@ $settings=mysqli_query($con,"select * from settings where settings_id='$sid'")or
  
   </body>
   </html>
-
-	
-  <script>
-function printSchedule() {
-  var monWedTable = document.getElementById("monWedTable").cloneNode(true);
-  var thuSatTable = document.getElementById("thuSatTable").cloneNode(true);
-
-  // Remove IDs from the cloned tables
-  monWedTable.removeAttribute("id");
-  thuSatTable.removeAttribute("id");
-
-  // Remove the "txtedit" and "txtdelete" elements from the cloned tables
-  var editElements = monWedTable.getElementsByClassName("txtedit");
-  for (var i = 0; i < editElements.length; i++) {
-    editElements[i].remove();
-  }
-
-  var deleteElements = monWedTable.getElementsByClassName("txtdelete");
-  for (var i = 0; i < deleteElements.length; i++) {
-    deleteElements[i].remove();
-  }
-
-  editElements = thuSatTable.getElementsByClassName("txtedit");
-  for (var i = 0; i < editElements.length; i++) {
-    editElements[i].remove();
-  }
-
-  deleteElements = thuSatTable.getElementsByClassName("txtdelete");
-  for (var i = 0; i < deleteElements.length; i++) {
-    deleteElements[i].remove();
-  }
-
-  // Create a new window for printing
-  var newWin = window.open("", "Print-Window");
-  newWin.document.open();
-  newWin.document.write("<html><head></head><body>");
-  newWin.document.write(monWedTable.outerHTML);
-  newWin.document.write("<br>");
-  newWin.document.write(thuSatTable.outerHTML);
-  newWin.document.write("</body></html>");
-  newWin.document.close();
-
-  // Print the contents and close the new window
-  newWin.onload = function() {
-    newWin.print();
-    newWin.close();
-  };
-}
-</script>
-
